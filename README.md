52c0db43485beb2e398a75e8743032fc5a6d864d
hr-205 <50244980+Hrodingar@users.noreply.github.com>
2020-02-22 11:16:15 +0300

    litle update

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 <h1>Why is this program created?</h1>
 
 <p>No need! This is a long-invented program that can be used as example code for beginner programmers.</p>
+
+<h2>What exactly can it be useful for novice programmers?</h2>
+
+<ul>
+	<li>
+		Using Functions
+	</li>
+	<li>
+		Using if-elif-else
+	</li>
+	<li>
+		Using modul random
+	</li>
+</ul>
--- guess_the_number.py
+++ guess_the_number.py
@@ -8,31 +8,39 @@ user_number = int(input())
 
 random_number = randrange(0, 50)
 
-def restart():
+# Function for restart or quit in this program
+def restart_or_quit():
+	print("For restart enter 'r', but if you want to exit enter 'q'")
+
 	restart_or_quit = input()
+
 	if restart_or_quit == 'r':
 		print("Please enter your number: ")
 		user_number = int(input()) 
+
 		start(user_number, random_number)
+
+	# In this branch will exit the program
 	elif restart_or_quit == 'q':
 		exit()
 	else:
 		print("Command not recognized, enter it again.")
 
-		restart()
+		restart_or_quit()
+
+# Function for start this program
 def start(user_number, random_number):
 	if user_number == random_number:
 			print("Yeah! You guessed.")
+	# In this branch, the program restarts
 	else:
 		if user_number > random_number:
 			print("Oh no, but your number was more than conceived. " + "Next time you will definitely succeed.\n")
-			print("For restart enter 'r', but if you want to exit enter 'q'")
 
-			restart()
+			restart_or_quit()
 		else:
 			print("Oh no, but your number was less than conceived. " + "Next time you will definitely succeed.\n")
-			print("For restart enter 'r', but if you want to exit enter 'q'")
 
-			restart()
+			restart_or_quit()
 
 start(user_number, random_number)
